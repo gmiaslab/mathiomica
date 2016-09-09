5e43883c79bcb6c3a9ebae778cc272ba94cc4240
gmiaslab <gmiaslab@users.noreply.github.com>
2016-09-09 11:56:24 -0400

    MathIOmica version 1.0 release

---
+++ MathIOmica_Source_Code/MathIOmica/PacletInfo.m
@@ -0,0 +1,22 @@
+(* Paclet Info File *)
+
+(* created 2016/08/26*)
+
+Paclet[
+    Name -> "MathIOmica",
+    Version -> "1.0.1",
+    MathematicaVersion -> "10+",
+    Description -> "The MIT License (MIT). MathIOmica is a general omics data analysis framework.  MathIOmica, Copyright (c) 2016 George I. Mias, G. Mias Lab, Department of Biochemistry and Molecular Biology, Michigan State University, East Lansing 48824.\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.",
+    Creator -> "George I.Mias Lab",
+    Extensions -> 
+        {
+            {"Kernel", Context -> 
+                {"MathIOmica`"}
+            }, 
+            {"Documentation", Resources -> 
+                {"MathIOmica/guide/MathIOmicaGuide", "MathIOmica/tutorial/MathIOmicaOverview", "MathIOmica/tutorial/MathIOmicaTutorial"}
+            , Language -> "English", LinkBase -> "MathIOmica", MainPage -> "Tutorials/MathIOmicaOverview"}
+        }
+]
+
+
